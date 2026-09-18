f931bbf61a45895b8b62d546ca5f747f9be8b03a
JasonGM01 <jgonzalezmolina3@toromail.csudh.edu>
2026-09-17 23:48:51 -0700

    fixed bug

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,20 @@
 import './App.css'
 import Board from "../src/game//board/board";
 import StartBoard from '../src/game/board/startBoard';
-import Movement from './game/movement/movement';
-import Knight from './game/movement/knightMove';
-import Rook from './game/movement/rookMove';
+import MovePiece from './game/movement/movePiece';
 
 function App() {
   const board = Board();
   const start = StartBoard(board);
 
   console.log(start);
-  console.log(Movement(start, 0, 1, 2, 2) ? "Movement is free" : "Movement is blocked");
-  console.log(`Movement is ${Knight(0,1,2,2)}`);
+  console.log(MovePiece(start, 0, 1, 2, 2) ? "Movement is free" : "Movement is blocked");
 
-  console.log(Movement(start, 0, 1, 1, 2) ? "Movement is free" : "Movement is blocked");
-  console.log(`Movement is ${Knight(0,1,1,2)}`);
+  console.log(MovePiece(start, 0, 1, 1, 2) ? "Movement is free" : "Movement is blocked");
 
-  console.log(Movement(start, 0, 0, 0, 2) ? "Movement is free" : "Movement is blocked");
-  console.log(`Movement is ${Rook(0,0,0,2)}`);
+  console.log(MovePiece(start, 0, 0, 0, 2) ? "Movement is free" : "Movement is blocked");
 
-  console.log(Movement(start, 0, 0, 0, 6) ? "Movement is free" : "Movement is blocked");
-  console.log(`Movement is ${Rook(0,0,0,6)}`);
+  console.log(MovePiece(start, 0, 0, 0, 6) ? "Movement is free" : "Movement is blocked");
 
   return (
     <div className="board">
